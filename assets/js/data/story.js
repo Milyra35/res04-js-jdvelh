085222d83b5fd6a81a524e41b22d9614cc9805ce
Milyra35 <marie.richir35@gmail.com>
2023-06-22 22:43:05 +0200

    Début exercice jeu dont vous êtes le héros

---
+++ assets/js/data/story.js
@@ -0,0 +1,30 @@
+let story = [
+    {
+        intro : "Vous êtes un sorcier devant se débarasser de Lucifer afin que la Terre soit sauvée.",
+        question : "Vous êtes devant un démon primordial, que faites-vous ?",
+        choix : [
+            {
+                text : "Vous essayez de le battre",
+                destination : {}
+            },
+            {
+                text : "Vous revenez en arrière, vous aller mourir si vous essayez de le tuer",
+                destination : {}
+            }
+        ]
+    }
+    {
+        intro : "Le démon meurt, après des heures à lui envoyer des boules de feu",
+        question : "Vous avez deux chemins possibles qui s'offrent à vous ensuite :",
+        choix : [
+            {
+                text : "A droite, un chemin vers la ville la plus proche",
+                destination : {}
+            },
+            {
+                text : "A gauche, une forêt sombre dont vous ne voyez pas la fin",
+                destination : {}
+            }
+        ]
+    }
+]
